ddfedd41b3d9b908762c6fd5a2ffe09ea43df68a
딘 <jhlee@lunasoft.co.kr>
2020-01-14 11:12:31 +0900

    init HystrixDashboard

---
+++ HystrixDashboard/src/test/java/kr/co/lunasoft/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.lunasoft;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
